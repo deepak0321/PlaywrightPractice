d4fd9e3e6a33aa92c0173af12b08872aab535140
Deepak Raj Gnanamuralidharan(UST <291493@ust.com>
2025-04-21 15:08:24 +0530

    some concepts

--- tests/EaseMyTrip.spec.js
+++ tests/EaseMyTrip.spec.js
@@ -12,5 +12,11 @@ test.only('EaseMyTrip Booking', async ({browser}) => {
     await page.locator("input#txtdesticity").click();
     await page.locator("input#txtdesticity").fill("Chengalpattu");
     await page.getByText('Chengalpattu(CGL)').click();
+    await page.locator('[name="txtDate"]').click();
+    await page.locator('.ui-state-default', { hasText: '30' }).first().click();
+    await page.locator('#SearchAll').click();
+    await page.locator("[data-val='2S']").first().click();
+    await page.locator('.bk_nw').first().click();
+    
     await page.waitForTimeout(5000);
 });
